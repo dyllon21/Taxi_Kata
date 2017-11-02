958ec03aa15fcaa7f51d7483f216e71566ac83bc
Dyllon <isaacsdyllon123@gmail.com>
2017-11-02 14:58:49 +0200

    making the taxi move with reference to the traffic light

--- index.js
+++ index.js
@@ -3,13 +3,42 @@ var body = document.querySelector("body");
 
 body.onkeydown = function(e) {
 
-  if (e.keyCode == 39) {
+  if (e.keyCode === 39) {
     var robots = taxi.closestTrafficLight();
-
-    if (robots.currentColor() == 'green') {
-
+    if (robots.currentColor() !== "red") {
+      // robots.makeGreen();
       taxi.forward();
+    // }else{
+    //   robots.makeOrange();
+    //
     }
   }
+  if (e.keyCode === 37) {
+    var robots = taxi.closestTrafficLight();
+    if (robots.currentColor() !== "red") {
+      taxi.reverse();
+    }
+  }
+   else if (e.keyCode === 38) {
+    var robots = taxi.closestTrafficLight();
 
+    if (robots.currentColor() === "green") {
+      robots.makeOrange();
+    } else if (robots.currentColor() === "orange") {
+      robots.makeRed();
+    }
+    else {
+      robots.makeGreen();
+  }
+}
+  else if (e.keyCode === 40) {
+    var robots = taxi.closestTrafficLight();
+    if (robots.currentColor() === "red") {
+      robots.makeGreen();
+    }else if(robots.currentColor() === "green"){
+      robots.makeOrange();
+    }else{
+      robots.makeRed();
+    }
+  }
 }
